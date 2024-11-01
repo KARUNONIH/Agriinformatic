4071332a99d458a49b51cb63a48eeceded1437f4
KARUNONIH <gibran.anggalana21@gmail.com>
2024-11-01 14:46:11 +0700

    add cover page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: ["./index.html", "./src/**/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      display: ["Oswald"],
+      body: ['"Open Sans"'],
+      bubble: ["Bubble Bobble", "sans-serif"],
+      rainstrom: ["Rainstorm", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
 };
-
-
